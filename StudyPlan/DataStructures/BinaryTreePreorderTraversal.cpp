8665673053393a902fe375ab5c0339af7e1354a2
Sivasubramani <t.sivasubramani.5@gmail.com>
2021-10-13 01:50:20 +0530

    https://leetcode.com/problems/binary-tree-preorder-traversal/
    https://leetcode.com/problems/binary-tree-preorder-traversal/

---
+++ StudyPlan/DataStructures/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,81 @@
+// 144. Binary Tree Preorder Traversal
+// Easy
+
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+// Example 1:
+
+// Input: root = [1,null,2,3]
+// Output: [1,2,3]
+
+// Example 2:
+
+// Input: root = []
+// Output: []
+
+// Example 3:
+
+// Input: root = [1]
+// Output: [1]
+
+// Example 4:
+
+// Input: root = [1,2]
+// Output: [1,2]
+
+// Example 5:
+
+// Input: root = [1,null,2]
+// Output: [1,2]
+
+ 
+
+// Constraints:
+
+//     The number of nodes in the tree is in the range [0, 100].
+//     -100 <= Node.val <= 100
+
+ 
+
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+  
+class Solution {
+public:
+    void preorder(TreeNode* root,vector<int> &res){
+        if(root == nullptr) return;
+        res.push_back(root->val);
+        preorder(root->left,res);
+        preorder(root->right,res);
+        
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root == nullptr) return{};
+        vector<int> res;
+        // preorder(root,res); //dfs
+        stack<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* x = q.top();
+            q.pop();
+            res.push_back(x->val);
+            if(x->right) q.push(x->right);
+            if(x->left) q.push(x->left);
+        }
+        return res;
+    }
+};
