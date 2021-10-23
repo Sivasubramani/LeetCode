5872292370b2f637551cd25470399faa4d644ad4
Sivasubramani <t.sivasubramani.5@gmail.com>
2021-10-24 00:48:31 +0530

    https://leetcode.com/problems/find-duplicate-subtrees/
    https://leetcode.com/problems/find-duplicate-subtrees/

---
+++ Google/FindDuplicateSubtrees.cpp
@@ -0,0 +1,59 @@
+// 652. Find Duplicate Subtrees
+// Medium
+
+// Given the root of a binary tree, return all duplicate subtrees.
+
+// For each kind of duplicate subtrees, you only need to return the root node of any one of them.
+
+// Two trees are duplicate if they have the same structure with the same node values.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,4,null,2,4,null,null,4]
+// Output: [[2,4],[4]]
+// Example 2:
+
+
+// Input: root = [2,1,1]
+// Output: [[1]]
+// Example 3:
+
+
+// Input: root = [2,2,2,3,null,3,null]
+// Output: [[2,3],[3]]
+ 
+
+// Constraints:
+
+// The number of the nodes in the tree will be in the range [1, 10^4]
+// -200 <= Node.val <= 200
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string dfs(TreeNode* root,vector<TreeNode*> &res,unordered_map<string,int> &m){
+        if(root == nullptr) return "";
+        string s = to_string(root->val)+" "+dfs(root->left,res,m)+" "+dfs(root->right,res,m);
+        if(m[s]++ ==1) res.push_back(root);
+        return s;
+    }
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        vector<TreeNode*> res;
+        unordered_map<string,int> m;
+        dfs(root,res,m);
+        return res;
+    }
+};
